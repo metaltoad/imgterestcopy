1e583aaf04e1566edd32b0f182168385900d295d
Joe Matthies <joematthies@MacBook-Pro.local>
2020-01-31 16:37:57 -0800

    Update React and add test code to verify it was updated live

--- src/App.js
+++ src/App.js
@@ -13,6 +13,9 @@ class App extends Component {
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <p className="App-intro">
+          The app has been updated to React 16.12
+        </p>
       </div>
     );
   }
